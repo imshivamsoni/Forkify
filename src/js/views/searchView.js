4b45d2192aad7f41516f0396c797dbd50bf75fc2
Shivam Soni <shivam@Shivams-MacBook-Air.local>
2022-07-26 19:33:22 +0530

    added clear input

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,6 +5,10 @@ class SearchView {
     return this.#parentEl.querySelector('.search__field').value;
   }
 
+  clearInput() {
+    return (this.#parentEl.querySelector('.search__field').value = '');
+  }
+
   addHandlerSearch(handler) {
     this.#parentEl.addEventListener('submit', function (e) {
       e.preventDefault();
