5c5939b0aae1a1c6d9236cfee523dde37cfe3e66
Shivam Soni <shivam@Shivams-MacBook-Air.local>
2022-07-29 21:12:25 +0530

    Fractional Error

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,8 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+//import { Fraction } from 'fractional';
+var Fraction = require('fractional').Fraction;
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
